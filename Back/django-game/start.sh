81c75b18d46e36349d686ae329c73a7a405c9b62
Ubuntu <ubuntu@ip-172-26-3-204.ap-northeast-2.compute.internal>
2021-09-14 05:44:41 +0000

    S05P21B306-301 [MODIFY] : 도커 파일 세팅, bash 파일, django 세팅 변경

--- Back/django-game/start.sh
+++ Back/django-game/start.sh
@@ -13,4 +13,4 @@ echo "==> 패키지 다운로드"
 pip install -r /code/requirements.txt
 
 echo "==> 배포!"
-gunicorn -b 0.0.0.0:8000 --env DJANGO_SETTINGS_MODULE=test_page.settings test_page.wsgi:application
+gunicorn -b 0.0.0.0:8000 --env DJANGO_SETTINGS_MODULE=django_game.settings django_game.wsgi:application
