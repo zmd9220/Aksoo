ed3a0ec3897bf7eb6f80030b7893a469637ab067
zmd9220 <zmd9220@gmail.com>
2021-09-24 16:54:23 +0900

    S05P21B306-333 [MODIFY] : 학습하기 단어 자음 모음 분리 코드 추가 requirements.txt에 한글 자모 분리를 위한 haugul-utils 라이브러리 추가

--- Back/django-game/learn/views.py
+++ Back/django-game/learn/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth import get_user_model
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
-
+from hangul_utils import split_syllables, join_jamos
 
 # Create your views here.
 @api_view(['GET'])
@@ -17,12 +17,16 @@ def get_words(request, select):
     if select == 'con':
         response_word = get_list_or_404(Alphabet, type=1)
         serializer = AlphabetSerializer(response_word, many=True)
-        # get_object_or_404()
     elif select == 'vow':
         response_word = get_list_or_404(Alphabet, type=2)
         serializer = AlphabetSerializer(response_word, many=True)
     else:
         response_word = Word.objects.all()[:10]
+        print(response_word)
+        for word in response_word:
+            # tmp = split_syllables(word.word)
+            # print(tmp)
+            word.word = split_syllables(word.word)
         serializer = WordSerializer(response_word, many=True)
     return Response(serializer.data)    
     
