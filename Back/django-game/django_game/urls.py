9c278df71707b62759f5a9540d4c5c829194192c
zmd9220 <zmd9220@gmail.com>
2021-09-27 14:17:50 +0900

    S05P21B306-373 [ADD] : 행맨 단어 제공 API 구현

--- Back/django-game/django_game/urls.py
+++ Back/django-game/django_game/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('learn/', include('learn.urls')),
+    path('games/', include('games.urls')),
 ]
--- Back/django-game/games/views.py
+++ Back/django-game/games/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+# from .serializer import UserDetailSerializer, UserSerializer
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from .models import User, Word, Hangman, AcidRain, CardMatching
+from .serializer import WordSerializer, HangmanSerializer, AcidRainSerializer, CardMatchingSerializer
+from django.contrib.auth import get_user_model
+
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from hangul_utils import split_syllables, join_jamos
 
 # Create your views here.
+@api_view(['GET'])
+def hangman(request, select):
+    # 해당 장르에 맞는 단어 리스트 가져오기
+    response_word = get_list_or_404(Word, theme_id=select)
+    for word in response_word:
+        # 각 단어별로 자모 분리
+        word.word = split_syllables(word.word)
+    # 단어 모음을 json화
+    serializer = WordSerializer(response_word, many=True)
+    # 응답
+    return Response(serializer.data) 
+
