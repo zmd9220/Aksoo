9ab5e4e43cefb6a61a47164dedf34048029feaaf
zmd9220 <zmd9220@gmail.com>
2021-10-03 20:44:08 +0900

    S05P21B306-387 [MODIFY] : Django 모든 url 뒤에 /추가

--- Back/django-game/games/urls.py
+++ Back/django-game/games/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'games'
 
 urlpatterns = [
-    path('hangman/<int:select>', views.hangman, name='hangman'),
-    path('setScore/<int:select_game>', views.set_score, name='set_score'),
-    path('ranking/<int:select_game>', views.ranking, name='ranking'),
+    path('hangman/<int:select>/', views.hangman, name='hangman'),
+    path('setScore/<int:select_game>/', views.set_score, name='set_score'),
+    path('ranking/<int:select_game>/', views.ranking, name='ranking'),
 ]
