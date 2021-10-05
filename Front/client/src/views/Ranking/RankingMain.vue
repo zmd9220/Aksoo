2a2f115455d220aac704218777fabb360dee2e7f
zmd9220 <zmd9220@gmail.com>
2021-10-05 16:49:55 +0900

    S05P21B306-419 [MODIFY] : 랭킹 페이지 오타, 코드 수정

--- Front/client/src/views/Ranking/RankingMain.vue
+++ Front/client/src/views/Ranking/RankingMain.vue
@@ -82,7 +82,6 @@
         <img src="@/assets/Learn/LearnStart/shape-2.svg" class="Shape-2" />
     </div>
 
-  </div>
 </template>
 <script>
 import RankingList from "@/views/Ranking/RankingList.vue";
