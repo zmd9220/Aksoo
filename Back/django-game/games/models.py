8545e0bd187096a21b8256d97e91b375a1e503dd
zmd9220 <zmd9220@gmail.com>
2021-10-06 22:25:21 +0900

    S05P21B306-419 [MODIFY] : 점수가 참조무결성 문제로 user넣고 저장이 안되는 문제 발생, 게임 DB 날리고 재생성

--- Back/django-game/games/models.py
+++ Back/django-game/games/models.py
@@ -4,31 +4,31 @@ from django.conf import settings
 
 # Create your models here.
 class AcidRain(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, models.DO_NOTHING)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     # user = models.ForeignKey(User, models.DO_NOTHING)
     score = models.IntegerField()
 
-    class Meta:
-        managed = False
-        db_table = 'acid_rain'
+    # class Meta:
+    #     managed = False
+    #     db_table = 'acid_rain'
 
 class CardMatching(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, models.DO_NOTHING)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
     # user = models.ForeignKey(User, models.DO_NOTHING)
     score = models.IntegerField()
 
-    class Meta:
-        managed = False
-        db_table = 'card_matching'
+    # class Meta:
+        # managed = False
+        # db_table = 'card_matching'
 
 
 class Hangman(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, models.DO_NOTHING)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     # user = models.ForeignKey(User, models.DO_NOTHING)
     score = models.IntegerField()
 
-    class Meta:
-        managed = False
-        db_table = 'hangman'
+    # class Meta:
+    #     managed = False
+    #     db_table = 'hangman'
 
 
