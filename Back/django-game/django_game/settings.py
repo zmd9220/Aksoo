589d25049d02bcb50681eccc0e913debe8c55538
zmd9220 <zmd9220@gmail.com>
2021-10-18 10:26:48 +0900

    211018 [MODIFY] : settings.py의 DB 부분 my_settings.py 로 이관, ignore에 추가
    Former-commit-id: f206fb3e4cf8919dc8a364dc0a11f797d0e8c4a3

--- Back/django-game/django_game/settings.py
+++ Back/django-game/django_game/settings.py
@@ -18,6 +18,8 @@ import datetime
 import pymysql
 pymysql.install_as_MySQLdb()
 
+from . import my_settings
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -115,18 +117,13 @@ WSGI_APPLICATION = 'django_game.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # Your Database
+    # Example
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'b306',
-        'USER': 'root',
-        'PASSWORD': 'b306',
-        'HOST': 'j5b306.p.ssafy.io',
-        'PORT': '3300',
-    }
+    'default': my_settings.default
 }
 
 
