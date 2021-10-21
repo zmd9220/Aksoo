71d07e4499934877ec477adf784acd95145048f1
zmd9220 <zmd9220@gmail.com>
2021-10-21 12:17:09 +0900

    211021 [MODIFY] : settings.py 미묘한 수정
    Former-commit-id: f3aaf0cdadd61e877e701e8d6a3dbca8cd4acfb8

--- Back/django-game/django_game/settings.py
+++ Back/django-game/django_game/settings.py
@@ -117,12 +117,12 @@ WSGI_APPLICATION = 'django_game.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # Your Database
     # Example
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    # Your Database
     'default': my_settings.default
 }
 
